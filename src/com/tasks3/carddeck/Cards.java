34a7de2cb26d89bad1a4578b9e098541cea07a2d
levro <levro@ukr.net>
2017-03-06 17:22:48 +0200

    1st commit. structure created

---
+++ src/com/tasks3/carddeck/Cards.java
@@ -0,0 +1,17 @@
+package com.tasks3.carddeck;
+
+public class Cards {
+
+	public static void main(String[] args) {
+		
+		Card hh = new Card(Rank.SIX, Suit.CLUBS);
+		System.out.println(hh.getRank().getName());
+		
+		Card[] pack;
+		pack = new Card[36];
+		pack[1] = new Card(Rank.ACE, Suit.CLUBS);
+		
+		System.out.println(pack[1].getRank().getName());
+	}
+
+}
