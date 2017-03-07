d81d5f3550d67a8f4107354bf85083a45d5319c0
levro <levro@ukr.net>
2017-03-07 14:59:30 +0200

    All working

--- src/com/tasks3/carddeck/Deck.java
+++ src/com/tasks3/carddeck/Deck.java
@@ -2,10 +2,11 @@ package com.tasks3.carddeck;
 
 public class Deck {
 	Card[] deck;
-    int index = 0;
+    int index;
     
 	// Constructor	
-	public Deck(){    	
+	public Deck(){
+	    index = 35;
 		int i = -1;
 		this.deck = new Card[Suit.values.length * Rank.values.length];
 	    for (Suit suit: Suit.values) {
@@ -18,12 +19,12 @@ public class Deck {
     //Перемішує колоду у випадковому порядку 
     public void shuffle() {
     	Card tmpCard;
-    	int q = (int) (Math.random()*10000+40000);
+    	int q = (int) (Math.random()*1000+4000);
     	for (int n=0; n<q; n++){
-    		int m = (int) (Math.random()*31+5);
+    		int m = (int) (Math.random()*28+8);
     		tmpCard = this.deck[m];
-    		this.deck[m] = this.deck[m-5];
-    		this.deck[m-5] = tmpCard;
+    		this.deck[m] = this.deck[m-8];
+    		this.deck[m-8] = tmpCard;
     	}
     }
     /* * Впорядкування колоди за мастями та значеннями 
@@ -42,7 +43,8 @@ public class Deck {
     * HEARTS 6 
     * І так далі для DIAMONDS, CLUBS, SPADES */
     public void order() {
-		int i = -1;
+		index = 35;
+    	int i = -1;
 		this.deck = new Card[Suit.values.length * Rank.values.length];
 	    for (Suit suit: Suit.values) {
 	        for (Rank rank: Rank.values) {
@@ -53,53 +55,52 @@ public class Deck {
     
     //Повертає true у випадку коли в колоді ще доступні карти
     public boolean hasNext() {
-    	return false;
+    	return index > -1;
     }
     
     //"Виймає" одну карту з колоди, коли буде видано всі 36 карт повертає null
     //Карти виймаються з "вершини" колоди. Наприклад перший виклик видасть SPADES 6 потім
     //SPADES 7, ..., CLUBS 6, ..., CLUBS Ace і так далі до HEARTS Ace
     public Card drawOne() {
-    	return new Card(Rank.SIX, Suit.SPADES);
+    	if (index >= 0)
+        	return this.deck[index--];
+        else return null;
     }
     
     public void deckPrn(){
-	    for (int j=0; j<36; j++){
+    	System.out.println("---Cards in deck-"+(index+1)+"---");
+	    for (int j=0; j<index+1; j++){
+			System.out.print("|");
 			System.out.print(this.deck[j].getSuit().getName());
 			System.out.print(" ");
 			System.out.println(this.deck[j].getRank().getName());
-	    }	
+	    }
     }
+	
+    public void cardPrn(Card crd){
+    	if (crd != null){
+    		System.out.print(crd.getSuit().getName());
+    		System.out.print(" ");
+    		System.out.println(crd.getRank().getName());
+    	}
+    	else System.out.println("Card is NULL");
+    }
+    
     
 	public static void main(String[] args) {
 				
 		Deck dk1 = new Deck();
-		
-		/*
-		Card[] dk;
-		dk = new Card[36];
-		dk[1] = new Card(Rank.ACE, Suit.CLUBS);
-		//System.out.println(dk1.deck.length);
-		//System.out.println(pack[1].getRank().getName());
-		
-		int i = -1;
-	    for (Suit suit: Suit.values) {
-	        for (Rank rank: Rank.values) {
-	            dk[++i] = new Card(rank,suit);
-	        }
-	    }*/
-		
-/*		dk1.deckPrn();
-		System.out.println("---------------------1------------------------");
-		dk1.deck[3].setRank(Rank.SIX);
-		dk1.deck[2].setRank(Rank.SIX);		
-		dk1.deckPrn();
-		System.out.println("---------------------2------------------------");
-		dk1.order();*/
-		dk1.shuffle();
-		dk1.deckPrn();
+
+		//dk1.deckPrn();
 		System.out.println("---------------------3------------------------");
-		dk1.order();
+		//dk1.order();
+		//dk1.cardPrn(dk1.deck[10]);
+		
+		for (int t=0; t<36; t++){
+			dk1.cardPrn(dk1.drawOne());
+			System.out.println("----------------------");	
+		}
+		System.out.println(dk1.hasNext());
 		dk1.deckPrn();
 	}
     
